769fa05512e1581fca60ab79218e6d629358307e
Abhinav <abhinavroy2099@gmail.com>
2024-10-06 13:17:32 +0530

    update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: true, // This allows all origins
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   await app.listen(8000);
 }
 bootstrap();
