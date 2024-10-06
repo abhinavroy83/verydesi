09d8bebff42b360f2bdb1abf05a0f6f4721ecc7b
Abhinav <abhinavroy2099@gmail.com>
2024-10-06 14:06:59 +0530

    update_orign

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: [
+      'http://localhost:3000',
+      'http://ec2-18-237-230-139.us-west-2.compute.amazonaws.com/',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     preflightContinue: false,
     optionsSuccessStatus: 204,
