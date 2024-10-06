1eb518783a850b7b2c62b3a299cf91e01dbd41cf
Abhinav <abhinavroy2099@gmail.com>
2024-10-06 13:02:17 +0530

    update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,16 +4,7 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:3000', // Localhost
-      'http://ec2-18-237-230-139.us-west-2.compute.amazonaws.com', // EC2 public DNS
-      'http://18.237.230.139', // EC2 public IP
-    ],
-    methods: ['GET', 'POST', 'PATCH', 'PUT'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(8000);
 }
 bootstrap();
