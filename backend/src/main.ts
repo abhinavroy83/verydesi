5afcd3171092f424f0a97f56ddda7ad10443dca5
Abhinav <abhinavroy2099@gmail.com>
2024-10-06 10:24:52 +0530

    update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,8 +10,6 @@ async function bootstrap() {
       'http://ec2-18-237-230-139.us-west-2.compute.amazonaws.com',
       'http://18.237.230.139',
     ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
   });
   await app.listen(8000);
 }
