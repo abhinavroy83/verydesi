9083a26caf265ffab6a834bfab2f6dd2a7364335
Abhinav <abhinavroy2099@gmail.com>
2024-10-06 10:56:33 +0530

    update_t3

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,8 @@ async function bootstrap() {
       'http://localhost:3000', // Add this if you're running the frontend locally
       'https://webmasterbee.com', // Your production domain
       'http://apiv2.verydesi.com',
-      "http://ec2-18-237-230-139.us-west-2.compute.amazonaws.com" // Backend domain (if necessary)
+      'http://ec2-18-237-230-139.us-west-2.compute.amazonaws.com',
+      'http://18.237.230.139',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
