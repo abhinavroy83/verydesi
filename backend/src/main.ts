a370bee2663005fffee02320b5230c5fbe1c9a4b
Abhinav <abhinavroy2099@gmail.com>
2024-10-06 10:07:42 +0530

    update_cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,15 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://webmasterbee.com',
+      'ec2-18-237-230-139.us-west-2.compute.amazonaws.com/',
+      '18.237.230.139',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(8000);
 }
 bootstrap();
