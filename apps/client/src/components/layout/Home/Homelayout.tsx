c767716df841a5cc4e459330f27eedb46a69d74b
Abhinav <abhinavroy2099@gmail.com>
2024-12-31 00:11:55 +0530

    update_category

--- apps/client/src/components/layout/Home/Homelayout.tsx
+++ apps/client/src/components/layout/Home/Homelayout.tsx
@@ -2,7 +2,7 @@
 import { Card, CardContent } from "@/components/ui/card";
 import dynamic from "next/dynamic";
 import { usePathname } from "next/navigation";
-import React, { ReactNode } from "react";
+import React, { ReactNode, use } from "react";
 const LeafletMap = dynamic(() => import("@/components/map/LeafletMap"), {
   ssr: false,
 });
@@ -27,13 +27,50 @@ export function HomeLayout({ children }: { children: ReactNode }) {
       "Phone & Cable",
     ],
   };
+  const pathname = usePathname();
 
-  const businesspath= pathname.startsWith ===='business'
+  const businesspath = pathname === "/business";
   return (
     <div className="flex lg:flex-row flex-col bg-background max-w-[1370px] lg:max-w-[1600px] mt-[8rem] mx-auto px-4 sm:px-6">
       <div className="w-full lg:w-4/5 mr-4">{children}</div>
-      <div className="w-full lg:max-w-[300px] max-w-full lg:ml-0 h-[725px] flex flex-col gap-6">
+      <div
+        className={`w-full lg:max-w-[300px] max-w-full lg:ml-0  ${businesspath ? "h-full" : "h-[725px]"} flex flex-col gap-6`}
+      >
         <LeafletMap />
+        {businesspath && (
+          <Card className="mt-3">
+            <CardContent className="p-0">
+              {/* <h2 className="text-[25px] font-bold p-4 pb-2">Services &</h2> */}
+              <div className="bg-yellow-400 rounded-lg p-1">
+                <div className="p-1 space-y-2">
+                  <div className="space-y-1">
+                    {Object.entries(categories).map(([section, items]) => (
+                      <div key={section} className="space-y-4">
+                        <h3 className="text-xl font-semibold text-muted-foreground">
+                          {section}
+                        </h3>
+                        <div className="flex flex-wrap gap-1">
+                          {items.map((category) => (
+                            <div
+                              key={category}
+                              // variant="secondary"
+                              className="cursor-pointer bg-white p-1 px-2 rounded-full hover:bg-primary hover:text-primary-foreground transition-colors"
+                            >
+                              {category}
+                            </div>
+                          ))}
+                        </div>
+                        <p className="flex w-full justify-end cursor-pointer">
+                          View All Category
+                        </p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        )}
         <div
           onClick={() => {
             window.open("https://g.co/fi/r/Y2A1CA", "_blank");
