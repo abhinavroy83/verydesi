f2a6483d84b79fa8d07a448681055f2e78df302c
Abhinav <abhinavroy2099@gmail.com>
2025-01-21 09:55:50 +0530

    add post-events route to middleware authentication check

--- apps/client/src/middleware.ts
+++ apps/client/src/middleware.ts
@@ -31,7 +31,7 @@ export async function middleware(request: NextRequest) {
   if (
     !token &&
     (url.pathname.startsWith("/dashboard") ||
-      url.pathname.startsWith("/post-room"))
+      url.pathname.startsWith("/post-room")|| url.pathname.startsWith("/post-events") ) 
   ) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
